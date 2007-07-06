953d2b890344ed95259c210d6ab8bcceb9ed8281
andre.moelle <andre.moelle@b58856fa-8134-0410-a1bd-ef8086d935a4>
2007-07-06 18:38:02 +0000

    Global-classes added for improved testing-capabilities.

---
+++ trunk/lib/MVCLite/Request/Global.php
@@ -0,0 +1,166 @@
+<?php
+/**
+ * MVCLite
+ * 
+ * LICENSE
+ * 
+ * The new BSD license is applied on this source-file. For further
+ * information please visit http://license.nordic-dev.de/newbsd.txt
+ * or send an email to andre.moelle@gmail.com.
+ */
+
+/**
+ * This class is used as base-class for containers storing superglobals.
+ * 
+ * These container-classes provide the possibility for a better testing.
+ * It can be applied especially for testing the controllers.
+ * 
+ * @category   MVCLite
+ * @package    Request
+ * @copyright  2007 Nordic Development
+ * @license    http://license.nordic-dev.de/newbsd.txt (New-BSD license)
+ * @author     Andre Moelle <andre.moelle@gmail.com>
+ * @version    $Id:$
+ */
+abstract class MVCLite_Request_Global
+{
+	/**
+	 * Children which this class contains.
+	 * 
+	 * @var array
+	 */
+	protected $_children = null;
+	
+	/**
+	 * Since this class is a singleton, the constructor is hidden.
+	 * 
+	 * @param array|null $global content of the superglobal
+	 */
+	protected function __construct ($global = null)
+	{
+		$this->set($global);
+	}
+	
+	/**
+	 * Since this class should not be cloned, the __clone-method is hidden.
+	 */
+	protected function __clone ()
+	{
+		;
+	}
+	
+	/**
+	 * Returns the child with the given name.
+	 * 
+	 * @param string $name name of the child to return
+	 * @return mixed
+	 */
+	public function __get ($name)
+	{
+		$children = $this->getChildren();
+		
+		if(!isset($children[$name]))
+		{
+			return null;
+		}
+		
+		return $children[$name];
+	}
+	
+	/**
+	 * Checks whether a property of this class is set.
+	 * 
+	 * @param string $name variable to check
+	 * @return boolean
+	 */
+	public function __isset ($name)
+	{
+		$children = $this->getChildren();
+		
+		return isset($children[$name]);
+	}
+	
+	/**
+	 * Sets a new variable and possibly overwrites an old one.
+	 * 
+	 * @param string $name name of the variable
+	 * @param mixed $value value of the new variable
+	 */
+	public function __set ($name, $value)
+	{
+		$children = $this->getChildren();
+		$children[$name] = $value;
+		
+		$this->set($children);
+	}
+	
+	/**
+	 * Unsets a value.
+	 * 
+	 * @param string $name name of the variable to unset
+	 */
+	public function __unset ($name)
+	{
+		if(isset($this->$name))
+		{
+			unset($this->_children[$name]);
+		}
+	}
+	
+	/**
+	 * Returns the superglobal variable for this class.
+	 * 
+	 * @return array
+	 */
+	abstract protected function _getGlobal ();
+	
+	/**
+	 * Returns the children of this class.
+	 * 
+	 * If the children do not exist the default globals are
+	 * taken instead.
+	 * 
+	 * @return array
+	 */
+	public function getChildren ()
+	{
+		if($this->_children == null)
+		{
+			$this->_children = $this->_getGlobal();
+		}
+		
+		return $this->_children;
+	}
+	
+	/**
+	 * Name of the superglobal.
+	 * 
+	 * @return string
+	 */
+	abstract public function getName ();
+	
+	/**
+	 * Global classes should implement the singleton pattern.
+	 * 
+	 * This method is used to return an instance of a global
+	 * class.
+	 * 
+	 * @param array|null $global content of the superglobal
+	 * @return MVCLite_Request_Global
+	 */
+	abstract public static function getInstance ($global = null);
+	
+	/**
+	 * Sets new content.
+	 * 
+	 * @param array|null $array array representing the new content
+	 * @return MVCLite_Request_Global
+	 */
+	public function set ($array)
+	{
+		$this->_children = $array;
+		
+		return $this;
+	}
+}
+?>
---
+++ trunk/lib/MVCLite/Request/Global/Session.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * MVCLite
+ * 
+ * LICENSE
+ * 
+ * The new BSD license is applied on this source-file. For further
+ * information please visit http://license.nordic-dev.de/newbsd.txt
+ * or send an email to andre.moelle@gmail.com.
+ */
+
+require_once 'MVCLite/Request/Global.php';
+require_once 'MVCLite/Request/Global/Synchronizable.php';
+
+/**
+ * This class represents the container for session-variables.
+ * 
+ * @category   MVCLite
+ * @package    Request
+ * @copyright  2007 Nordic Development
+ * @license    http://license.nordic-dev.de/newbsd.txt (New-BSD license)
+ * @author     Andre Moelle <andre.moelle@gmail.com>
+ * @version    $Id:$
+ */
+class MVCLite_Request_Global_Session extends MVCLite_Request_Global implements MVCLite_Request_Global_Synchronizable
+{
+	/**
+	 * Global instance.
+	 * 
+	 * @var MVCLite_Request_Global
+	 */
+	private static $_instance;
+	
+	/**
+	 * Starts the session.
+	 * 
+	 * @see MVCLite_Request_Global::__construct()
+	 */
+	protected function __construct ($content = null)
+	{
+		if(!defined('PHPUnit_MAIN_METHOD'))
+		{
+			session_start();
+		}
+		
+		parent::__construct($content);
+	}
+	
+	/**
+	 * @see MVCLite_Request_Global::_getGlobal()
+	 */
+	protected function _getGlobal ()
+	{
+		if(!isset($_SESSION))
+		{
+			return array();
+		}
+		
+		return $_SESSION;
+	}
+	
+	/**
+	 * @see MVCLite_Request_Global::getInstance()
+	 */
+	public static function getInstance ($content = null)
+	{
+		if(self::$_instance == null)
+		{
+			self::$_instance = new self($content);
+		}
+		
+		return self::$_instance;
+	}
+	
+	/**
+	 * @see MVCLite_Request_Global_Synchronizable::synchronize()
+	 */
+	public function synchronize ()
+	{
+		$children = $this->getChildren();
+		
+		if($children == array() || $this->_getGlobal() == $children)
+		{
+			return false;
+		}
+		
+		$_SESSION = $children;
+		
+		return true;
+	}
+	
+	/**
+	 * @see MVCLite_Request_Global::getName()
+	 */
+	public function getName ()
+	{
+		return 'session';
+	}
+}
+?>
