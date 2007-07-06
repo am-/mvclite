e321cc5cdbcb81185ccd3def17f080c131175c89
andre.moelle <andre.moelle@b58856fa-8134-0410-a1bd-ef8086d935a4>
2007-07-06 18:38:02 +0000

    Global-classes added for improved testing-capabilities.

---
+++ lib/MVCLite/Request/Global/Server.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * MVCLite
+ * 
+ * LICENSE
+ * 
+ * The new BSD license is applied on this source-file. For further
+ * information please visit http://license.nordic-dev.de/newbsd.txt
+ * or send an email to andre.moelle@gmail.com.
+ */
+
+require_once 'MVCLite/Request/Global.php';
+
+/**
+ * This class represents the container for server-variables.
+ * 
+ * @category   MVCLite
+ * @package    Request
+ * @copyright  2007 Nordic Development
+ * @license    http://license.nordic-dev.de/newbsd.txt (New-BSD license)
+ * @author     Andre Moelle <andre.moelle@gmail.com>
+ * @version    $Id:$
+ */
+class MVCLite_Request_Global_Server extends MVCLite_Request_Global
+{
+	/**
+	 * Global instance.
+	 * 
+	 * @var MVCLite_Request_Global
+	 */
+	private static $_instance;
+	
+	/**
+	 * @see MVCLite_Request_Global::_getGlobal()
+	 */
+	protected function _getGlobal ()
+	{
+		return $_SERVER;
+	}
+	
+	/**
+	 * @see MVCLite_Request_Global::getInstance()
+	 */
+	public static function getInstance ($content = null)
+	{
+		if(self::$_instance == null)
+		{
+			self::$_instance = new self($content);
+		}
+		
+		return self::$_instance;
+	}
+	
+	/**
+	 * @see MVCLite_Request_Global::getName()
+	 */
+	public function getName ()
+	{
+		return 'server';
+	}
+}
+?>
---
+++ lib/MVCLite/Request/Global/Synchronizable.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * MVCLite
+ * 
+ * LICENSE
+ * 
+ * The new BSD license is applied on this source-file. For further
+ * information please visit http://license.nordic-dev.de/newbsd.txt
+ * or send an email to andre.moelle@gmail.com.
+ */
+
+/**
+ * Interface for classes which synchronize with their superglobal.
+ * 
+ * @category   MVCLite
+ * @package    Request
+ * @copyright  2007 Nordic Development
+ * @license    http://license.nordic-dev.de/newbsd.txt (New-BSD license)
+ * @author     Andre Moelle <andre.moelle@gmail.com>
+ * @version    $Id:$
+ */
+interface MVCLite_Request_Global_Synchronizable
+{
+	/**
+	 * Synchronizes its content with the superglobal-variable.
+	 * 
+	 * @return boolean
+	 */
+	public function synchronize ();
+}
+?>
