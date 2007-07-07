50495caa5960682f8449a93edfdf237bcdab07ef
andre.moelle <andre.moelle@b58856fa-8134-0410-a1bd-ef8086d935a4>
2007-07-07 22:10:41 +0000

    Adaptable-interface added.

---
+++ trunk/lib/MVCLite/Db/Exception.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * MVCLite
+ * 
+ * LICENSE
+ * 
+ * The new BSD license is applied on this source-file. For further
+ * information please visit http://license.nordic-dev.de/newbsd.txt
+ * or send an email to andre.moelle@gmail.com.
+ */
+
+require_once 'MVCLite/Exception.php';
+
+/**
+ * Exception which is used within the db-component of MVCLite.
+ * 
+ * @category   MVCLite
+ * @package    Db
+ * @copyright  2007 Nordic Development
+ * @license    http://license.nordic-dev.de/newbsd.txt (New-BSD license)
+ * @author     Andre Moelle <andre.moelle@gmail.com>
+ * @version    $Id:$
+ */
+class MVCLite_Db_Exception extends MVCLite_Exception
+{
+	
+}
+?>
