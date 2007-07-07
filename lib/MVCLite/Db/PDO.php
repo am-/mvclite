a3ac5d2899a3ac1c3ed7dc70edb08e1f7969a5d1
andre.moelle <andre.moelle@b58856fa-8134-0410-a1bd-ef8086d935a4>
2007-07-07 22:15:03 +0000

    PDO-object implemented.

---
+++ lib/MVCLite/Db/PDO.php
@@ -0,0 +1,178 @@
+<?php
+/**
+ * MVCLite
+ * 
+ * LICENSE
+ * 
+ * The new BSD license is applied on this source-file. For further
+ * information please visit http://license.nordic-dev.de/newbsd.txt
+ * or send an email to andre.moelle@gmail.com.
+ */
+
+require_once 'MVCLite/Db/Adaptable.php';
+
+/**
+ * This is the PDO-adapter which can be used by MVCLite.
+ * 
+ * Mainly it directs the method-calls to the PDO-object.
+ * 
+ * @category   MVCLite
+ * @package    Db
+ * @copyright  2007 Nordic Development
+ * @license    http://license.nordic-dev.de/newbsd.txt (New-BSD license)
+ * @author     Andre Moelle <andre.moelle@gmail.com>
+ * @version    $Id:$
+ */
+class MVCLite_Db_PDO implements MVCLite_Db_Adaptable
+{
+	/**
+	 * Instance of the PDO-instance.
+	 * 
+	 * @var PDO
+	 */
+	protected $_pdo;
+	
+	/**
+	 * Information about the database-connection.
+	 * 
+	 * These information are extracted from the constructor and
+	 * are used to create the PDO-object.
+	 * 
+	 * @var array
+	 */
+	protected $_connection = array();
+	
+	/**
+	 * Creates the instance of the PDO-class.
+	 * 
+	 * @param string $dsn information about the connection
+	 * @param string $username username
+	 * @param string $password password
+	 * @param array $options driver-specific options
+	 */
+	public function __construct ($dsn, $username = '', $password = '', array $options = array())
+	{
+		$array = func_get_args();
+		
+		for($i = 0, $c = func_num_args(); $i < $c; $i++)
+		{
+			if(!$array[$i])
+			{
+				break;
+			}
+		}
+		
+		$this->_connection = array_slice($array, 0, $i);
+	}
+	
+	/**
+	 * Returns the PDO-instance.
+	 * 
+	 * Because this adapter can be created without need for database
+	 * operations, the PDO-class is instantiated only when it is
+	 * needed.
+	 * 
+	 * @return PDO
+	 */
+	protected function pdo ()
+	{
+		if($this->_pdo == null)
+		{
+			$class = new ReflectionClass('PDO');
+			$this->_pdo = $class->newInstanceArgs($this->_connection);
+		}
+		
+		return $this->_pdo;
+	}
+	
+	/**
+	 * @see MVCLite_Db_Adaptable::beginTransaction()
+	 */
+	public function beginTransaction ()
+	{
+		return $this->pdo()->beginTransaction();
+	}
+	
+	/**
+	 * @see MVCLite_Db_Adaptable::commit()
+	 */
+	public function commit ()
+	{
+		return $this->pdo()->commit();
+	}
+	
+	/**
+	 * @see MVCLite_Db_Adaptable::errorCode()
+	 */
+	public function errorCode ()
+	{
+		return $this->pdo()->errorCode();
+	}
+	
+	/**
+	 * @see MVCLite_Db_Adaptable::errorInfo()
+	 */
+	public function errorInfo ()
+	{
+		return $this->pdo()->errorInfo();
+	}
+	
+	/**
+	 * @see MVCLite_Db_Adaptable::execute()
+	 */
+	public function execute ($statement)
+	{
+		return $this->pdo()->execute($statement);
+	}
+	
+	/**
+	 * @see MVCLite_Db_Adaptable::lastInsertId()
+	 */
+	public function lastInsertId ($name = null)
+	{
+		if($name)
+		{
+			return $this->pdo()->lastInsertId($name);
+		}
+		
+		return $this->pdo()->lastInsertId();
+	}
+	
+	/**
+	 * @see MVCLite_Db_Adaptable::prepare()
+	 */
+	public function prepare ($statement, array $options = array())
+	{
+		if($options)
+		{
+			return $this->pdo()->prepare($statement, $options);
+		}
+		
+		return $this->pdo()->prepare($statement);
+	}
+	
+	/**
+	 * @see MVCLite_Db_Adaptable::query()
+	 */
+	public function query ($statement)
+	{
+		return $this->pdo()->query($statement);
+	}
+	
+	/**
+	 * @see MVCLite_Db_Adaptable::quote()
+	 */
+	public function quote ($string)
+	{
+		return $this->pdo()->quote($string);
+	}
+	
+	/**
+	 * @see MVCLite_Db_Adaptable::rollBack()
+	 */
+	public function rollBack ()
+	{
+		return $this->pdo()->rollBack();
+	}
+}
+?>
