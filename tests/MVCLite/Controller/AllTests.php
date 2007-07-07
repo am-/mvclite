b17d2b6c49901ebbc35c60789aeff1f1ea19905b
andre.moelle <andre.moelle@b58856fa-8134-0410-a1bd-ef8086d935a4>
2007-07-07 13:49:00 +0000

    Tests for controllers added.

---
+++ tests/MVCLite/Controller/AllTests.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * MVCLite
+ * 
+ * LICENSE
+ * 
+ * The new BSD license is applied on this source-file. For further
+ * information please visit http://license.nordic-dev.de/newbsd.txt
+ * or send an email to andre.moelle@gmail.com.
+ */
+
+require_once 'PHPUnit/Framework/TestSuite.php';
+require_once 'PHPUnit/TextUI/TestRunner.php';
+
+require_once 'MVCLite/ControllerTest.php';
+
+/**
+ * Runs all tests in the view-component.
+ * 
+ * @category   MVCLite
+ * @package    View
+ * @subpackage UnitTests
+ * @copyright  2007 Nordic Development
+ * @license    http://license.nordic-dev.de/newbsd.txt (New-BSD license)
+ * @author     Andre Moelle <andre.moelle@gmail.com>
+ * @version    $Id:$
+ */
+class MVCLite_View_AllTests
+{
+	/**
+	 * Runs the tests.
+	 * 
+	 * @return void
+	 */
+	public static function main ()
+	{
+		PHPUnit_TextUI_TestRunner::run(self::suite());
+	}
+	
+	/**
+	 * Returns the test-suite containing all tests to execute.
+	 * 
+	 * @return PHPUnit_Framework_TestSuite
+	 */
+	public static function suite ()
+	{
+		$suite = new PHPUnit_Framework_TestSuite('MVCLite - Controller tests');
+		
+		$suite->addTestSuite('MVCLite_ControllerTest');
+		
+		return $suite;
+	}
+}
+?>
