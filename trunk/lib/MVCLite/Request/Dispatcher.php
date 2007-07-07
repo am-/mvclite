e055b1bbb37395d4db89eba27e72408de2707743
andre.moelle <andre.moelle@b58856fa-8134-0410-a1bd-ef8086d935a4>
2007-07-07 09:17:34 +0000

    Dispatcher completed.

---
+++ trunk/lib/MVCLite/Request/Dispatcher.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * MVCLite
+ * 
+ * LICENSE
+ * 
+ * The new BSD license is applied on this source-file. For further
+ * information please visit http://license.nordic-dev.de/newbsd.txt
+ * or send an email to andre.moelle@gmail.com.
+ */
+
+require_once 'MVCLite/Loader.php';
+require_once 'MVCLite/Request.php';
+require_once 'MVCLite/Request/Dispatcher/Exception.php';
+require_once 'MVCLite/Request/Route.php';
+
+/**
+ * The dispatcher parses a request and proxies it to the controller.
+ * 
+ * The controller finalizes the dispatching-process.
+ * 
+ * @category   MVCLite
+ * @package    Request
+ * @copyright  2007 Nordic Development
+ * @license    http://license.nordic-dev.de/newbsd.txt (New-BSD license)
+ * @author     Andre Moelle <andre.moelle@gmail.com>
+ * @version    $Id:$
+ */
+class MVCLite_Request_Dispatcher
+{
+	/**
+	 * Route the dispatcher uses.
+	 * 
+	 * @var MVCLite_Request_Route
+	 */
+	protected $_route;
+	
+	/**
+	 * Constructor sets the dispatcher up.
+	 * 
+	 * @param MVCLite_Request_Route $route standard route
+	 */
+	public function __construct (MVCLite_Request_Route $route)
+	{
+		$this->setRoute($route);
+	}
+	
+	/**
+	 * Dispatches the request.
+	 * 
+	 * Firstly the URL is parsed to a request-object. Thereafter
+	 * the information parsed are used to create the correct
+	 * controller, which is dispatched after then.
+	 * 
+	 * There are some reasons for a failing dispatch-process. When the
+	 * requested controller does not exist a "MVCLite_Request_Dispatcher_Exception"
+	 * will be thrown. If the requested action does not exist a
+	 * "MVCLite_Controller_Exception" will be thrown.
+	 * 
+	 * @param string $url url to dispatch
+	 * @return boolean
+	 * @throws MVCLite_Request_Dispatcher_Exception
+	 * @throws MVCLite_Controller_Exception
+	 */
+	public function dispatch ($url)
+	{
+		$request = $this->getRoute()->parse($url);
+		
+		try
+		{
+			MVCLite_Loader::loadController($request->getController());
+			$class = $request->getController() . MVCLite_Loader::getSuffix(MVCLite_Loader::CONTROLLER);
+			
+			$controller = new $class();
+			$controller->dispatch($request);
+			
+			return true;
+		}
+		catch(MVCLite_Loader_Exception $e)
+		{
+			throw new MVCLite_Request_Dispatcher_Exception(
+				'Controller "' . $request->getController() . '" was not found'
+			);
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * Returns the active route.
+	 * 
+	 * @return MVCLite_Request_Route
+	 */
+	public function getRoute ()
+	{
+		return $this->_route;
+	}
+	
+	/**
+	 * Sets a new route-object.
+	 * 
+	 * @param MVCLite_Request_Route $route new route
+	 * @return MVCLite_Request_Dispatcher
+	 */
+	public function setRoute (MVCLite_Request_Route $route)
+	{
+		$this->_route = $route;
+		
+		return $this;
+	}
+}
+?>
---
+++ trunk/lib/MVCLite/Request/Dispatcher/Exception.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * MVCLite
+ * 
+ * LICENSE
+ * 
+ * The new BSD license is applied on this source-file. For further
+ * information please visit http://license.nordic-dev.de/newbsd.txt
+ * or send an email to andre.moelle@gmail.com.
+ */
+
+require_once 'MVCLite/Request/Exception.php';
+
+/**
+ * Exception which is used within the dispatcher of the request-component of MVCLite.
+ * 
+ * @category   MVCLite
+ * @package    Request
+ * @copyright  2007 Nordic Development
+ * @license    http://license.nordic-dev.de/newbsd.txt (New-BSD license)
+ * @author     Andre Moelle <andre.moelle@gmail.com>
+ * @version    $Id:$
+ */
+class MVCLite_Request_Dispatcher_Exception extends MVCLite_Request_Exception
+{
+	
+}
+?>
