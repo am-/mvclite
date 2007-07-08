9ee1f9fce0f7fbda280833dc5d33f8acf66ce95d
andre.moelle <andre.moelle@b58856fa-8134-0410-a1bd-ef8086d935a4>
2007-07-08 06:57:21 +0000

    Test for MVCLite_Db added.

---
+++ tests/MVCLite/DbTest.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * MVCLite
+ * 
+ * LICENSE
+ * 
+ * The new BSD license is applied on this source-file. For further
+ * information please visit http://license.nordic-dev.de/newbsd.txt
+ * or send an email to andre.moelle@gmail.com.
+ */
+
+include 'setUp.php';
+require_once 'MVCLite/Db.php';
+require_once 'MVCLite/Db/Exception.php';
+require_once 'MVCLite/Db/PDO.php';
+
+/**
+ * Unit-testing for the PDO-database adapter.
+ * 
+ * @category   MVCLite
+ * @package    Db
+ * @subpackage UnitTests
+ * @copyright  2007 Nordic Development
+ * @license    http://license.nordic-dev.de/newbsd.txt (New-BSD license)
+ * @author     Andre Moelle <andre.moelle@gmail.com>
+ * @version    $Id:$
+ */
+class MVCLite_DbTest extends PHPUnit_Framework_TestCase
+{
+	public function testClass ()
+	{
+		$db = MVCLite_Db::getInstance();
+		
+		try
+		{
+			$db->setAdapter(null)->getAdapter();
+			$this->assertTrue(
+				false,
+				'Getting of an unset adapter should thrown an exception'
+			);
+		}
+		catch (MVCLite_Db_Exception $e)
+		{
+			;
+		}
+		
+		$this->assertEquals(
+			'MVCLite_Db_PDO',
+			get_class($db->setAdapter(new MVCLite_Db_PDO('unimportant'))->getAdapter()),
+			'Class of set adapter does not match'
+		);
+	}
+}
+?>
