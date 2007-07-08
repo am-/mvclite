81c1382e8b8e19c84ca119574a1622cfebf0c54f
andre.moelle <andre.moelle@b58856fa-8134-0410-a1bd-ef8086d935a4>
2007-07-08 06:33:12 +0000

    Tests for the PDO-adapter written.

---
+++ trunk/tests/MVCLite/Db/AllTests.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * MVCLite
+ * 
+ * LICENSE
+ * 
+ * The new BSD license is applied on this source-file. For further
+ * information please visit http://license.nordic-dev.de/newbsd.txt
+ * or send an email to andre.moelle@gmail.com.
+ */
+
+require_once 'PHPUnit/Framework/TestSuite.php';
+require_once 'PHPUnit/TextUI/TestRunner.php';
+
+require_once 'MVCLite/Db/PDOTest.php';
+
+/**
+ * Runs all tests in the core.
+ * 
+ * @category   MVCLite
+ * @package    Db
+ * @subpackage UnitTests
+ * @copyright  2007 Nordic Development
+ * @license    http://license.nordic-dev.de/newbsd.txt (New-BSD license)
+ * @author     Andre Moelle <andre.moelle@gmail.com>
+ * @version    $Id:$
+ */
+class MVCLite_Db_AllTests
+{
+	/**
+	 * Runs the tests.
+	 * 
+	 * @return void
+	 */
+	public static function main ()
+	{
+		PHPUnit_TextUI_TestRunner::run(self::suite());
+	}
+	
+	/**
+	 * Returns the test-suite containing all tests to execute.
+	 * 
+	 * @return PHPUnit_Framework_TestSuite
+	 */
+	public static function suite ()
+	{
+		$suite = new PHPUnit_Framework_TestSuite('MVCLite - Database tests');
+		
+		$suite->addTestSuite('MVCLite_Db_PDOTest');
+		
+		return $suite;
+	}
+}
+?>
---
+++ trunk/tests/MVCLite/Db/PDOTest.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * MVCLite
+ * 
+ * LICENSE
+ * 
+ * The new BSD license is applied on this source-file. For further
+ * information please visit http://license.nordic-dev.de/newbsd.txt
+ * or send an email to andre.moelle@gmail.com.
+ */
+
+include 'setUp.php';
+require_once 'MVCLite/Db/Exception.php';
+require_once 'MVCLite/Db/PDO.php';
+
+/**
+ * Unit-testing for the PDO-database adapter.
+ * 
+ * @category   MVCLite
+ * @package    Db
+ * @subpackage UnitTests
+ * @copyright  2007 Nordic Development
+ * @license    http://license.nordic-dev.de/newbsd.txt (New-BSD license)
+ * @author     Andre Moelle <andre.moelle@gmail.com>
+ * @version    $Id:$
+ */
+class MVCLite_Db_PDOTest extends PHPUnit_Framework_TestCase
+{
+	public function testMySQL ()
+	{
+		// TODO: Add tests for transactions.
+		
+		$mysql = new MVCLite_Db_PDO('mysql:host=localhost;dbname=__mvclite', 'root', '');
+		
+		$this->assertFalse(
+			$mysql->isConnected(),
+			'PDO-adapter should not be connected'
+		);
+		
+		// this is necessary to fetch the correct last insert id
+		$mysql->execute('TRUNCATE TABLE `foobar` ');
+		
+		$this->assertEquals(
+			1,
+			$mysql->execute('INSERT INTO foobar (content) VALUES ("foobar")'),
+			'Insertion failed'
+		);
+		$this->assertTrue(
+			$mysql->isConnected(),
+			'PDO-adapter should be connected'
+		);
+		
+		$this->assertEquals(
+			1,
+			$mysql->lastInsertId(),
+			'1 should be last inserted id'
+		);
+		
+		$statement = $mysql->prepare('SELECT * FROM foobar');
+		
+		$this->assertEquals(
+			'PDOStatement',
+			get_class($statement),
+			'Prepared statement should be an instance of PDOStatement'
+		);
+		
+		$statement->execute();
+		
+		$this->assertEquals(
+			1,
+			count($statement->fetchAll()),
+			'Only one result should be returned'
+		);
+		
+		$this->assertEquals(
+			1,
+			$mysql->execute('INSERT INTO foobar (content) VALUES ("foobar")'),
+			'Insertion failed'
+		);
+		
+		try
+		{
+			$mysql->execute('UPDATE content = "bar" FROM foobar');
+			$this->assertTrue(false, 'Invalid query should throw an exception');
+		}
+		catch (MVCLite_Db_Exception $e)
+		{
+			
+		}
+		
+		$this->assertEquals(
+			2,
+			$mysql->execute('UPDATE foobar SET content = "bar" WHERE content = "foobar"'),
+			'This method should indicate that two rows are updated'
+		);
+		$this->assertEquals(
+			0,
+			$mysql->execute('UPDATE foobar SET content = "bar" WHERE content = "foobar"'),
+			'This method should indicate that no rows were updated'
+		);
+		
+		$this->assertEquals(
+			0,
+			count($mysql->query('SELECT * FROM foobar WHERE content = "foobar"')->fetchAll()),
+			'Select returned more than zero rows'
+		);
+		
+		$this->assertEquals(
+			'00000',
+			$mysql->errorCode(),
+			'If no errors occured, this method should return 00000'
+		);
+		$this->assertEquals(
+			array('00000'),
+			$mysql->errorInfo(),
+			'If no errors occured, this method should return an array with the element 00000'
+		);
+		
+		$this->assertEquals(
+			2,
+			$mysql->execute('DELETE FROM foobar WHERE 1'),
+			'One row should be deleted'
+		);
+	}
+}
+?>
