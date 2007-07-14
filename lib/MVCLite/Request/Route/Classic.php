451478e2c71dbda598e9e0c6cdacea7d71b490be
andre.moelle <andre.moelle@b58856fa-8134-0410-a1bd-ef8086d935a4>
2007-07-14 06:06:31 +0000

    Support for the classic route added. MVCLite is now able to use raw GET to apply MVC.

---
+++ lib/MVCLite/Request/Route/Classic.php
@@ -0,0 +1,143 @@
+<?php
+/**
+ * MVCLite
+ * 
+ * LICENSE
+ * 
+ * The new BSD license is applied on this source-file. For further
+ * information please visit http://license.nordic-dev.de/newbsd.txt
+ * or send an email to andre.moelle@gmail.com.
+ */
+
+/**
+ * This is the classic-route for servers unable to rewrite urls.
+ * 
+ * It is designed to work without url-rewriting or other hacks.
+ * Therefore it works with GET. Please refer to the following example
+ * how the query is built.
+ * 
+ * <code>
+ * controller=foo
+ * -> controller would be set to Foo
+ * action=bar
+ * -> action would be set to bar
+ * otherThanActionOrController=argument
+ * -> would added an argument with the specified name (otherThanActionOrController)
+ *    and the value "argument".
+ * </code>
+ * 
+ * @category   MVCLite
+ * @package    Request
+ * @copyright  2007 Nordic Development
+ * @license    http://license.nordic-dev.de/newbsd.txt (New-BSD license)
+ * @author     Andre Moelle <andre.moelle@gmail.com>
+ * @version    $Id$
+ */
+class MVCLite_Request_Route_Classic implements MVCLite_Request_Route
+{
+	/**
+	 * Name of the action-attribute in URL-strings.
+	 * 
+	 * @const string
+	 */
+	const ACTION = 'action';
+	
+	/**
+	 * Replacement-character for invalid characters.
+	 */
+	const CHARACTER = '-';
+	
+	/**
+	 * Name of the controller-attribute in URL-strings.
+	 * 
+	 * @const string
+	 */
+	const CONTROLLER = 'controller';
+	
+	/**
+	 * Prepares the key of a parameter.
+	 * 
+	 * The key is only allowed to contain characters and digits.
+	 * Therefore non applicable signs are filtered out.
+	 * 
+	 * @param string $key key which should be prepared
+	 * @return string
+	 */
+	protected function _prepareKey ($key)
+	{
+		return preg_replace('([^a-zA-Z0-9])', '', $key);
+	}
+	
+	/**
+	 * @see MVCLite_Request_Route::assemble()
+	 */
+	public function assemble (MVCLite_Request $request)
+	{
+		$array = array();
+		
+		foreach($request->getParams() as $key => $value)
+		{
+			if(is_numeric($key))
+			{
+				$key = $value;
+				$value = '';
+			}
+			
+			$array[$this->_prepareKey($key)] = $value;
+		}
+		
+		if($request->getController() != 'Index')
+		{
+			$array[self::CONTROLLER] = strtolower($request->getController());
+		}
+		if($request->getAction() != 'index')
+		{
+			$array[self::ACTION] = $request->getAction();
+		}
+		
+		$result = http_build_query($array);
+		
+		return ($result ? '?' : '') . $result;
+	}
+	
+	/**
+	 * @see MVCLite_Request_Route::parse()
+	 */
+	public function parse ($url)
+	{
+		$request = new MVCLite_Request($this);
+		
+		$query = parse_url($url, PHP_URL_QUERY);
+		$query = strtr($query, array_flip(get_html_translation_table()));
+		
+		if(!$query)
+		{
+			return $request;
+		}
+		
+		$args = array();
+		
+		foreach(explode('&', $query) as $part)
+		{
+			$temp = explode('=', $part);
+			
+			if($temp[0] == self::CONTROLLER && isset($temp[1]))
+			{
+				$request->setController($temp[1]);
+			}
+			else if($temp[0] == self::ACTION && isset($temp[1]))
+			{
+				$request->setAction($temp[1]);
+			}
+			else
+			{
+				$args[$temp[0]] = rawurldecode((isset($temp[1]) ? $temp[1] : ''));
+			}
+		}
+		
+		$request->setParams($args);
+		
+		return $request;
+	}
+}
+?>
---
+++ tests/MVCLite/Request/Route/ClassicTest.php
@@ -0,0 +1,189 @@
+<?php
+/**
+ * MVCLite
+ * 
+ * LICENSE
+ * 
+ * The new BSD license is applied on this source-file. For further
+ * information please visit http://license.nordic-dev.de/newbsd.txt
+ * or send an email to andre.moelle@gmail.com.
+ */
+
+include 'setUp.php';
+
+/**
+ * Tests the classic-route.
+ * 
+ * @category   MVCLite
+ * @package    Request
+ * @subpackage UnitTests
+ * @copyright  2007 Nordic Development
+ * @license    http://license.nordic-dev.de/newbsd.txt (New-BSD license)
+ * @author     Andre Moelle <andre.moelle@gmail.com>
+ * @version    $Id: RouteTest.php 84 2007-07-11 21:50:48Z andre.moelle $
+ */
+class MVCLite_Request_Route_ClassicTest extends PHPUnit_Framework_TestCase
+{
+	public function compare (MVCLite_Request $obj, MVCLite_Request $obj2)
+	{
+		foreach(array('getController', 'getAction', 'getParams') as $method)
+		{
+			if($obj->$method() != $obj2->$method())
+			{
+				var_dump($obj->$method());
+				var_dump($obj2->$method());
+				echo $method . '(' . $obj->$method() . '!=' . $obj2->$method() . ')';
+				return false;
+			}
+		}
+		
+		return true;
+	}
+
+	public function testAssembler ()
+	{
+		$route = new MVCLite_Request_Route_Classic();
+		$request = new MVCLite_Request($route);
+		
+		$this->assertEquals(
+			'',
+			$route->assemble(
+				$request->setController('Index')
+						->setAction('index')
+						->setParams(array(), MVCLite_Request::MODUS_RECREATE)
+			),
+			'index|index|null was not recognized correctly'
+		);
+		$this->assertEquals(
+			'?action=foobar',
+			$route->assemble(
+				$request->setController('Index')
+						->setAction('foobar')
+						->setParams(array(), MVCLite_Request::MODUS_RECREATE)
+			),
+			'index|foobar|null was not recognized correctly'
+		);
+		$this->assertEquals(
+			'?controller=foobar&action=foobar',
+			$route->assemble(
+				$request->setController('Foobar')
+						->setAction('foobar')
+						->setParams(array(), MVCLite_Request::MODUS_RECREATE)
+			),
+			'foobar|foobar|null was not recognized correctly'
+		);
+		$this->assertEquals(
+			'?id=42',
+			$route->assemble(
+				$request->setController('Index')
+						->setAction('index')
+						->setParams(array('id' => 42), MVCLite_Request::MODUS_RECREATE)
+			),
+			'index|index|id=42 was not recognized correctly'
+		);
+		$this->assertEquals(
+			'?id=42&foo=bar',
+			$route->assemble(
+				$request->setController('Index')
+						->setAction('index')
+						->setParams(array('id' => 42, 'foo' => 'bar'), MVCLite_Request::MODUS_RECREATE)
+			),
+			'index|index|id=42;foo=bar was not recognized correctly'
+		);
+		$this->assertEquals(
+			'?id=42&foo=bar&controller=bar&action=foo',
+			$route->assemble(
+				$request->setController('Bar')
+						->setAction('foo')
+						->setParams(array('id' => 42, 'foo' => 'bar'), MVCLite_Request::MODUS_RECREATE)
+			),
+			'bar|foo|id=42;foo=bar was not recognized correctly'
+		);
+		$this->assertEquals(
+			'?foo=foo+ist+b%E4r.&controller=bar&action=foo',
+			$route->assemble(
+				$request->setController('Bar')
+						->setAction('foo')
+						->setParams(array('foo' => 'foo ist bär.'), MVCLite_Request::MODUS_RECREATE)
+			),
+			'bar|foo|foo="foo ist bär" was not recognized correctly'
+		);
+		$this->assertEquals(
+			'?foo=foo+++bar&controller=bar&action=foo',
+			$route->assemble(
+				$request->setController('Bar')
+						->setAction('foo')
+						->setParams(array('foo' => 'foo   bar'), MVCLite_Request::MODUS_RECREATE)
+			),
+			'bar|foo|foo="foo   bar" was not recognized correctly'
+		);
+		$this->assertEquals(
+			'?foo=foo._bar&controller=bar&action=foo',
+			$route->assemble(
+				$request->setController('Bar')
+						->setAction('foo')
+						->setParams(array('foo' => 'foo._bar'), MVCLite_Request::MODUS_RECREATE)
+			),
+			'bar|foo|foo="foo._bar" was not recognized correctly'
+		);
+		$this->assertEquals(
+			'?somecontent=&controller=bar&action=foo',
+			$route->assemble(
+				$request->setController('Bar')
+						->setAction('foo')
+						->setParams(array('some content'), MVCLite_Request::MODUS_RECREATE)
+			),
+			'bar|foo|"some content" was not recognized correctly'
+		);
+	}
+	
+	public function testParser ()
+	{
+		$route = new MVCLite_Request_Route_Classic();
+		$request = new MVCLite_Request($route);
+		
+		$this->assertTrue(
+			$this->compare(
+				$request,
+				$route->parse('?')
+			),
+			'index|index|null parsing failed'
+		);
+		$this->assertTrue(
+			$this->compare(
+				$request,
+				$route->parse('index.php?controller=INDEX&action=INDEX')
+			),
+			'index|index|null parsing failed'
+		);
+		$this->assertTrue(
+			$this->compare(
+				$request->setController('bar'),
+				$route->parse('?controller=bar&action=INDEX')
+			),
+			'bar|index|null parsing failed'
+		);
+		$this->assertTrue(
+			$this->compare(
+				$request->setAction('foo'),
+				$route->parse('?controller=bar&action=foo')
+			),
+			'bar|foo|null parsing failed'
+		);
+		$this->assertTrue(
+			$this->compare(
+				$request->setParam('id', '42'),
+				$route->parse('?controller=bar&action=foo&id=42')
+			),
+			'bar|foo|id=42 parsing failed'
+		);
+		$this->assertTrue(
+			$this->compare(
+				$request->setParam('id', '42.bar')->setParam('foo', 'bar'),
+				$route->parse('?controller=bar&action=foo&id=42.bar&foo=bar')
+			),
+			'bar|foo|id="42.bar";foo=bar parsing failed'
+		);
+	}
+}
+?>
