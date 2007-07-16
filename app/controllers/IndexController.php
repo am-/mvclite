9f74cefde25a17d85d03c988f4abdd13948def0c
andre.moelle <andre.moelle@b58856fa-8134-0410-a1bd-ef8086d935a4>
2007-07-16 06:58:24 +0000

    Hello World-example added.

---
+++ app/controllers/IndexController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * MVCLite
+ * 
+ * LICENSE
+ * 
+ * The new BSD license is applied on this source-file. For further
+ * information please visit http://license.nordic-dev.de/newbsd.txt
+ * or send an email to andre.moelle@gmail.com.
+ */
+
+/**
+ * Hello World-example.
+ * 
+ * @category   MVCLite
+ * @package    Application
+ * @copyright  2007 Nordic Development
+ * @license    http://license.nordic-dev.de/newbsd.txt (New-BSD license)
+ * @author     Andre Moelle <andre.moelle@gmail.com>
+ * @version    $Id$
+ */
+class IndexController extends MVCLite_Controller_Abstract
+{
+	/**
+	 * Index-action prints "Hello World!" out.
+	 * 
+	 * Since the view automatically sets a template fitting to the
+	 * controller and action, the indexAction does not anything
+	 * other than existing.
+	 */
+	public function indexAction ()
+	{
+		
+	}
+}
+?>
