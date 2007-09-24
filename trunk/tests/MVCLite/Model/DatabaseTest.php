8d3af34ae3dd23f432cc6dbb34b7612e645f7576
andre.moelle <andre.moelle@b58856fa-8134-0410-a1bd-ef8086d935a4>
2007-09-24 21:40:32 +0000

    Database-specific code refactored out of the base-model.

--- trunk/tests/MVCLite/Model/DatabaseTest.php
+++ trunk/tests/MVCLite/Model/DatabaseTest.php
@@ -24,9 +24,9 @@ require_once 'MVCLite/Model/Abstract.php';
  * @copyright  2007 Nordic Development
  * @license    http://license.nordic-dev.de/newbsd.txt (New-BSD license)
  * @author     Andre Moelle <andre.moelle@gmail.com>
- * @version    $Id$
+ * @version    $Id: ModelTest.php 120 2007-07-15 13:35:26Z andre.moelle $
  */
-class MVCLite_ModelTest extends PHPUnit_Framework_TestCase
+class MVCLite_Model_DatabaseTest extends PHPUnit_Framework_TestCase
 {
 	public function testDatabase ()
 	{
@@ -101,7 +101,7 @@ class MVCLite_ModelTest extends PHPUnit_Framework_TestCase
 /*
  * Classes required for unittesting.
  */
-class ModeltestModel extends MVCLite_Model_Abstract
+class ModeltestModel extends MVCLite_Model_Abstract_Database
 {
 	
 }
